cd5de027f3fd4920ad593b02afd2a484266e0979
oyama <52624121+yoyama92@users.noreply.github.com>
2024-08-31 18:44:27 +0900

    muiを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,44 @@
+import { Box, Link } from "@mui/material";
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+export default () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
+    <Box
+      component="main"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "6rem",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "inherit",
+          justifyContent: "inherit",
+          alignItems: "inherit",
+          fontSize: "0.85rem",
+          maxWidth: "100%",
+          zIndex: 2,
+          fontFamily: "var(--font-mono)",
+        }}
+      >
+        <Box component="p">
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
+          <Box
+            component="code"
+            sx={{
+              fontWeight: 700,
+              fontFamily: "var(--font-mono)",
+            }}
+          >
+            src/app/page.tsx
+          </Box>
+        </Box>
+        <Box>
+          <Link
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -19,77 +47,89 @@ export default function Home() {
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
               width={100}
               height={24}
               priority={true}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+          </Link>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "0.85rem",
+          position: "relative",
+          padding: "4rem 0;",
+        }}
+      >
         <Image
-          className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority={true}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
+      </Box>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, minmax(25%, auto))",
+          maxWidth: "100%",
+          width: "var(--max-width)",
+          gap: "0.5rem",
+        }}
+      >
+        <Link
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
+          <Box component="h2">
+            Docs <Box component="span">-&gt;</Box>
+          </Box>
+          <Box component="p">
+            Find in-depth information about Next.js features and API.
+          </Box>
+        </Link>
+        <Link
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <Box component="h2">
+            Learn <Box component="span">-&gt;</Box>
+          </Box>
+          <Box component="p">
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </Box>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+          <Box component="h2">
+            Templates <Box component="span">-&gt;</Box>
+          </Box>
+          <Box component="p">Explore starter templates for Next.js.</Box>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
+          <Box component="h2">
+            Deploy <Box component="span">-&gt;</Box>
+          </Box>
+          <Box component="p">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Box>
+        </Link>
+      </Box>
+    </Box>
   );
-}
+};
